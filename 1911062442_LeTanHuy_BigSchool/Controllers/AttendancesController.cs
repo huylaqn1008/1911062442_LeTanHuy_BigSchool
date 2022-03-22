bc4a4016135e405b64752989335f11e89c226e5e
Huy Lê Tấn <garena281215@gmail.com>
2022-03-22 21:54:10 +0700

    commit 21:54 ngày 22/3/2022

--- 1911062442_LeTanHuy_BigSchool/Controllers/AttendancesController.cs
+++ 1911062442_LeTanHuy_BigSchool/Controllers/AttendancesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _1911062442_LeTanHuy_BigSchool.Models;
+using System;
+using Microsoft.AspNet.Identity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +9,25 @@ using System.Web.Http;
 
 namespace _1911062442_LeTanHuy_BigSchool.Controllers
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendance = new Attendance()
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId() 
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
     }
 }
